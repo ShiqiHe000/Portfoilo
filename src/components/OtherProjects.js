e9e5ff3e003d19b916d6a2d25d3b4baa93f2768c
Shiqi He <she010@uottawa.ca>
2021-07-03 16:35:07 -0400

    change other project animations

--- src/components/OtherProjects.js
+++ src/components/OtherProjects.js
@@ -1,45 +1,43 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import EverydayPost from "./EverydayPost";
 import HelloweenGame from "./HelloweenGame";
 import Calculator from "./Calculator";
-import {gsap} from 'gsap';
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 
 const OtherProjects = () => {
     useEffect(() => {
-
         gsap.registerPlugin(ScrollTrigger);
 
         const tl = gsap.timeline({
             scrollTrigger: {
-                trigger: '[data-other-projects]', 
-                start: "top center", 
-                scrub: true, 
+                trigger: "[data-other-projects]",
+                start: "top center",
+            },
+        });
+        tl.from(
+            "[data-other-projects]",
+            {
+                opacity: 0,
+                duration: 1,
             }
-        })
-        tl.fromTo('[data-other-projects]', {
-            opacity: 0
-        }, {
-            duration: 1, 
-            opacity: 1
-        })
-        tl.fromTo('[data-other-project]', {
-            opacity: 0, 
-            y: 50
-        }, {
-            opacity: 1, 
-            duration: 1, 
-            y: 0
-        })
-
-    }, [])
+         
+        );
+        tl.from("[data-other-project]", {
+            opacity: 0,
+            y: 50,
+            duration: 1,
+            stagger: 0.5,
+        });
+    }, []);
 
     return (
         <div className="container">
-            <h2 className="primaryTitle" data-other-projects>Other works</h2>
-            <EverydayPost /> 
-            <HelloweenGame /> 
+            <h2 className="primaryTitle" data-other-projects>
+                Other works
+            </h2>
+            <EverydayPost />
+            <HelloweenGame />
             <Calculator />
         </div>
     );
